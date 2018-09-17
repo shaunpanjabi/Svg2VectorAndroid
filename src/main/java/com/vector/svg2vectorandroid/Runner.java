51a84d980d176e629a5c3b667003cd6f2c66c7be
Klim Zavadsky <KlimZavadsky@coherentsolutions.com>
2018-09-17 14:49:01 +0300

    Added support of destinationVectorDirectory argument in command line.

--- src/main/java/com/vector/svg2vectorandroid/Runner.java
+++ src/main/java/com/vector/svg2vectorandroid/Runner.java
@@ -4,17 +4,30 @@ package com.vector.svg2vectorandroid;
  * Created by ravi on 19/12/17.
  */
 public class Runner {
-
     public static void main(String args[]){
-
-        if(args.length == 0){
-            System.out.println(" Provide source directory as first arguement for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath> ");
+        if (args.length == 0) {
+            System.out.println("Provide source directory as first arguement for svg files to be converted\n example: java -jar Svg2VectorAndroid-1.0.jar <SourceDirectoryPath>");
             return;
         }
 
         String sourceDirectory = args[0];
-        if(null != sourceDirectory && !sourceDirectory.isEmpty()){
-            SvgFilesProcessor processor = new SvgFilesProcessor(sourceDirectory);
+        SvgFilesProcessor processor = null;
+
+        if (args.length == 2) {
+            String destinationDirectory = args[1];
+
+            if (null != sourceDirectory && !sourceDirectory.isEmpty() &&
+                null != destinationDirectory && !destinationDirectory.isEmpty()) {
+                processor = new SvgFilesProcessor(sourceDirectory, destinationDirectory);
+            }
+        }
+        else {
+            if (null != sourceDirectory && !sourceDirectory.isEmpty()) {
+                processor = new SvgFilesProcessor(sourceDirectory);
+            }
+        }
+
+        if (processor != null) {
             processor.process();
         }
     }
